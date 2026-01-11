4a65f3bf3aa69831c4098c87f27c1653371595f2
Shou-Li Hsu <hank850503@gmail.com>
2026-01-11 04:26:59 -0800

    Extract system call handling to separate file (#85)
    - Add system_call_runner.hpp/cpp with RunSystemCall() entry point
    - Move all system call handlers ($display, $time, $finish, etc.)
    - Move HandleMemIO for $readmem*/$writemem* support
    - Move helper functions (FormatValue, TimeFormatContext, etc.)
    - Simplify instruction_runner.cpp kSystemCall case to single dispatch

---
+++ include/lyra/interpreter/system_call_runner.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <memory>
+
+#include "lyra/interpreter/instruction_result.hpp"
+
+namespace lyra::lir {
+struct Instruction;
+}  // namespace lyra::lir
+
+namespace lyra::interpreter {
+
+class InstanceContext;
+class ProcessEffect;
+class ProcessFrame;
+class SimulationContext;
+class TempTable;
+
+// Executes a system call instruction ($display, $time, $finish, etc.)
+// Returns the appropriate InstructionResult based on the system call.
+auto RunSystemCall(
+    const lir::Instruction& instr, SimulationContext& simulation_context,
+    ProcessFrame& frame, ProcessEffect& effect, TempTable& temp_table,
+    const std::shared_ptr<InstanceContext>& instance_context)
+    -> InstructionResult;
+
+}  // namespace lyra::interpreter
