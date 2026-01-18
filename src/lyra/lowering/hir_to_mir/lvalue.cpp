4b2cba251267802a134bc3dba4307d2ee825cd50
Shou-Li Hsu <hank850503@gmail.com>
2026-01-18 02:39:20 -0800

    Refactor lvalue.cpp to extract visit handlers
    - Extract LowerNameRefLvalue, LowerElementAccessLvalue, LowerMemberAccessLvalue
    - Matches expression.cpp pattern with named functions in anonymous namespace

--- src/lyra/lowering/hir_to_mir/lvalue.cpp
+++ src/lyra/lowering/hir_to_mir/lvalue.cpp
@@ -16,6 +16,65 @@
 
 namespace lyra::lowering::hir_to_mir {
 
+namespace {
+
+auto LowerNameRefLvalue(const hir::NameRefExpressionData& data, Context& ctx)
+    -> mir::PlaceId {
+  return ctx.LookupPlace(data.symbol);
+}
+
+auto LowerElementAccessLvalue(
+    const hir::ElementAccessExpressionData& data, MirBuilder& builder)
+    -> mir::PlaceId {
+  Context& ctx = builder.GetContext();
+
+  mir::PlaceId base_place = LowerLvalue(data.base, builder);
+  mir::Operand index_operand = LowerExpression(data.index, builder);
+
+  const mir::Place& base = (*ctx.mir_arena)[base_place];
+  TypeId base_type_id = mir::TypeOfPlace(*ctx.type_arena, base);
+  const Type& base_type = (*ctx.type_arena)[base_type_id];
+  if (base_type.Kind() != TypeKind::kUnpackedArray) {
+    throw common::InternalError(
+        "LowerElementAccessLvalue", "base is not an unpacked array");
+  }
+  if (index_operand.kind != mir::Operand::Kind::kConst &&
+      index_operand.kind != mir::Operand::Kind::kUse) {
+    throw common::InternalError(
+        "LowerElementAccessLvalue", "index operand must be Const or Use");
+  }
+
+  mir::Projection proj{
+      .kind = mir::Projection::Kind::kIndex,
+      .operand = index_operand,
+  };
+  return ctx.mir_arena->DerivePlace(base_place, std::move(proj));
+}
+
+auto LowerMemberAccessLvalue(
+    const hir::MemberAccessExpressionData& data, MirBuilder& builder)
+    -> mir::PlaceId {
+  Context& ctx = builder.GetContext();
+
+  mir::PlaceId base_place = LowerLvalue(data.base, builder);
+
+  const mir::Place& base = (*ctx.mir_arena)[base_place];
+  TypeId base_type_id = mir::TypeOfPlace(*ctx.type_arena, base);
+  const Type& base_type = (*ctx.type_arena)[base_type_id];
+  if (base_type.Kind() != TypeKind::kUnpackedStruct) {
+    throw common::InternalError(
+        "LowerMemberAccessLvalue", "base is not an unpacked struct");
+  }
+
+  mir::Projection proj{
+      .kind = mir::Projection::Kind::kField,
+      .operand = data.field_index,
+  };
+  return ctx.mir_arena->DerivePlace(base_place, std::move(proj));
+}
+
+}  // namespace
+
 auto LowerLvalue(hir::ExpressionId expr_id, MirBuilder& builder)
     -> mir::PlaceId {
   Context& ctx = builder.GetContext();
@@ -25,48 +84,13 @@ auto LowerLvalue(hir::ExpressionId expr_id, MirBuilder& builder)
       [&](const auto& data) -> mir::PlaceId {
         using T = std::decay_t<decltype(data)>;
         if constexpr (std::is_same_v<T, hir::NameRefExpressionData>) {
-          return ctx.LookupPlace(data.symbol);
+          return LowerNameRefLvalue(data, ctx);
         } else if constexpr (std::is_same_v<
                                  T, hir::ElementAccessExpressionData>) {
-          mir::PlaceId base_place = LowerLvalue(data.base, builder);
-          mir::Operand index_operand = LowerExpression(data.index, builder);
-
-          const mir::Place& base = (*ctx.mir_arena)[base_place];
-          TypeId base_type_id = mir::TypeOfPlace(*ctx.type_arena, base);
-          const Type& base_type = (*ctx.type_arena)[base_type_id];
-          if (base_type.Kind() != TypeKind::kUnpackedArray) {
-            throw common::InternalError(
-                "LowerLvalue", "ElementAccess base is not an unpacked array");
-          }
-          if (index_operand.kind != mir::Operand::Kind::kConst &&
-              index_operand.kind != mir::Operand::Kind::kUse) {
-            throw common::InternalError(
-                "LowerLvalue",
-                "ElementAccess index operand must be Const or Use");
-          }
-
-          mir::Projection proj{
-              .kind = mir::Projection::Kind::kIndex,
-              .operand = index_operand,
-          };
-          return ctx.mir_arena->DerivePlace(base_place, std::move(proj));
+          return LowerElementAccessLvalue(data, builder);
         } else if constexpr (std::is_same_v<
                                  T, hir::MemberAccessExpressionData>) {
-          mir::PlaceId base_place = LowerLvalue(data.base, builder);
-
-          const mir::Place& base = (*ctx.mir_arena)[base_place];
-          TypeId base_type_id = mir::TypeOfPlace(*ctx.type_arena, base);
-          const Type& base_type = (*ctx.type_arena)[base_type_id];
-          if (base_type.Kind() != TypeKind::kUnpackedStruct) {
-            throw common::InternalError(
-                "LowerLvalue", "MemberAccess base is not an unpacked struct");
-          }
-
-          mir::Projection proj{
-              .kind = mir::Projection::Kind::kField,
-              .operand = data.field_index,
-          };
-          return ctx.mir_arena->DerivePlace(base_place, std::move(proj));
+          return LowerMemberAccessLvalue(data, builder);
         } else {
           throw common::InternalError(
               "LowerLvalue", "unsupported lvalue expression");
