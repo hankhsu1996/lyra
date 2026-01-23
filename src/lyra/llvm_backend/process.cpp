911cbf704b5301429a8f9b5a4be0cf56b5390c56
Shou-Li Hsu <hank850503@gmail.com>
2026-01-23 01:25:25 -0800

    Refactor LLVM runtime API surface (#246)
    - Remove dead single-process LyraRunSimulation overload
    - Add LyraSuspendDelay/Wait/Repeat helpers (own layout)
    - Replace GEP+store patterns with suspend helper calls
    - Simplify SuspendRecord LLVM type to opaque blob
    - Rename LyraDesignStore* to LyraStorePacked/String
    - Bump kMaxInlineTriggers from 4 to 8

--- src/lyra/llvm_backend/process.cpp
+++ src/lyra/llvm_backend/process.cpp
@@ -12,13 +12,13 @@
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Type.h"
 #include "llvm/IR/Value.h"
-#include "llvm/Support/Casting.h"
 #include "lyra/common/overloaded.hpp"
 #include "lyra/common/unsupported_error.hpp"
 #include "lyra/llvm_backend/context.hpp"
 #include "lyra/llvm_backend/instruction.hpp"
 #include "lyra/mir/routine.hpp"
 #include "lyra/mir/terminator.hpp"
+#include "lyra/runtime/suspend_record.hpp"
 
 namespace lyra::lowering::mir_to_llvm {
 
@@ -75,62 +75,50 @@ void LowerDelay(
   auto& builder = context.GetBuilder();
   auto& llvm_ctx = context.GetLlvmContext();
 
-  llvm::Value* suspend_record = context.GetSuspendRecordPointer();
-
-  auto* i8_ty = llvm::Type::getInt8Ty(llvm_ctx);
-  auto* i32_ty = llvm::Type::getInt32Ty(llvm_ctx);
   auto* i64_ty = llvm::Type::getInt64Ty(llvm_ctx);
+  auto* i32_ty = llvm::Type::getInt32Ty(llvm_ctx);
 
-  auto* tag_ptr = builder.CreateStructGEP(
-      context.GetSuspendRecordType(), suspend_record, 0, "tag_ptr");
-  builder.CreateStore(llvm::ConstantInt::get(i8_ty, 1), tag_ptr);
-
-  auto* ticks_ptr = builder.CreateStructGEP(
-      context.GetSuspendRecordType(), suspend_record, 1, "ticks_ptr");
-  builder.CreateStore(llvm::ConstantInt::get(i64_ty, delay.ticks), ticks_ptr);
-
-  auto* resume_ptr = builder.CreateStructGEP(
-      context.GetSuspendRecordType(), suspend_record, 2, "resume_ptr");
-  builder.CreateStore(
-      llvm::ConstantInt::get(i32_ty, delay.resume.value), resume_ptr);
+  builder.CreateCall(
+      context.GetLyraSuspendDelay(),
+      {context.GetStatePointer(), llvm::ConstantInt::get(i64_ty, delay.ticks),
+       llvm::ConstantInt::get(i32_ty, delay.resume.value)});
 
   builder.CreateBr(exit_block);
 }
 
 void LowerWait(
     Context& context, const mir::Wait& wait, llvm::BasicBlock* exit_block) {
+  static_assert(sizeof(runtime::WaitTriggerRecord) == 8);
+
+  if (wait.triggers.size() > runtime::kMaxInlineTriggers) {
+    throw common::UnsupportedErrorException(
+        common::UnsupportedLayer::kMirToLlvm, common::UnsupportedKind::kFeature,
+        context.GetCurrentOrigin(),
+        std::format(
+            "too many wait triggers ({}, max {})", wait.triggers.size(),
+            runtime::kMaxInlineTriggers));
+  }
+
   auto& builder = context.GetBuilder();
   auto& llvm_ctx = context.GetLlvmContext();
-
-  llvm::Value* suspend_record = context.GetSuspendRecordPointer();
-  auto* suspend_type = context.GetSuspendRecordType();
 
   auto* i8_ty = llvm::Type::getInt8Ty(llvm_ctx);
   auto* i32_ty = llvm::Type::getInt32Ty(llvm_ctx);
 
-  // Write kWait (2) to tag
-  auto* tag_ptr = builder.CreateStructGEP(suspend_type, suspend_record, 0);
-  builder.CreateStore(llvm::ConstantInt::get(i8_ty, 2), tag_ptr);
-
-  // Write resume block
-  auto* resume_ptr = builder.CreateStructGEP(suspend_type, suspend_record, 2);
-  builder.CreateStore(
-      llvm::ConstantInt::get(i32_ty, wait.resume.value), resume_ptr);
-
-  // Write num_triggers
   auto num_triggers = static_cast<uint32_t>(wait.triggers.size());
-  auto* num_ptr = builder.CreateStructGEP(suspend_type, suspend_record, 3);
-  builder.CreateStore(llvm::ConstantInt::get(i32_ty, num_triggers), num_ptr);
 
-  // Write each trigger
-  auto* triggers_ptr = builder.CreateStructGEP(suspend_type, suspend_record, 4);
-  auto* array_type =
-      llvm::cast<llvm::ArrayType>(suspend_type->getElementType(4));
-  auto* trigger_type =
-      llvm::cast<llvm::StructType>(array_type->getElementType());
+  // WaitTriggerRecord layout: {i32 signal_id, i8 edge, [3 x i8] padding}
+  auto* trigger_type = llvm::StructType::get(
+      llvm_ctx, {i32_ty, i8_ty, llvm::ArrayType::get(i8_ty, 3)});
+  auto* array_type = llvm::ArrayType::get(trigger_type, num_triggers);
 
+  // Create local alloca for trigger array
+  auto* triggers_alloca = builder.CreateAlloca(array_type, nullptr, "triggers");
+
+  // Fill each trigger element
   for (uint32_t i = 0; i < num_triggers; ++i) {
-    auto* elem_ptr = builder.CreateConstGEP2_32(array_type, triggers_ptr, 0, i);
+    auto* elem_ptr =
+        builder.CreateConstGEP2_32(array_type, triggers_alloca, 0, i);
 
     // Write signal_id (field 0)
     auto* signal_ptr = builder.CreateStructGEP(trigger_type, elem_ptr, 0);
@@ -146,26 +134,21 @@ void LowerWait(
         edge_ptr);
   }
 
+  // Call LyraSuspendWait(state, resume_block, triggers, num_triggers)
+  builder.CreateCall(
+      context.GetLyraSuspendWait(),
+      {context.GetStatePointer(),
+       llvm::ConstantInt::get(i32_ty, wait.resume.value), triggers_alloca,
+       llvm::ConstantInt::get(i32_ty, num_triggers)});
+
   builder.CreateBr(exit_block);
 }
 
 void LowerRepeat(Context& context, llvm::BasicBlock* exit_block) {
   auto& builder = context.GetBuilder();
-  auto& llvm_ctx = context.GetLlvmContext();
 
-  llvm::Value* suspend_record = context.GetSuspendRecordPointer();
-  auto* suspend_type = context.GetSuspendRecordType();
-
-  auto* i8_ty = llvm::Type::getInt8Ty(llvm_ctx);
-  auto* i32_ty = llvm::Type::getInt32Ty(llvm_ctx);
-
-  // Write kRepeat (3) to tag
-  auto* tag_ptr = builder.CreateStructGEP(suspend_type, suspend_record, 0);
-  builder.CreateStore(llvm::ConstantInt::get(i8_ty, 3), tag_ptr);
-
-  // Write 0 to resume_block (restart at entry)
-  auto* resume_ptr = builder.CreateStructGEP(suspend_type, suspend_record, 2);
-  builder.CreateStore(llvm::ConstantInt::get(i32_ty, 0), resume_ptr);
+  builder.CreateCall(
+      context.GetLyraSuspendRepeat(), {context.GetStatePointer()});
 
   builder.CreateBr(exit_block);
 }
